89602fde7add5ba78f2ac9e4640914614960451e
Edwuin Elias Cortez Valeencia <ednland@hotmail.com>
2017-08-08 00:27:03 -0500

    test parte 2

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { TenantComponent } from './tenant/tenant.component';
-import { Test1Component } from './src/app/admin/test1/test1.component';
-import { Test2Component } from './src/app/admin/test2/test2.component';
-import { Test3Component } from './src/app/admin/test3/test3.component';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
+import { Test3Component } from './test3/test3.component';
 
 
 
